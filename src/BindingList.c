d0d6c75756deeda0c60fe588721d3b5831675180
florardu <andretabourian@hotmail.com>
2021-06-06 16:50:08 +0300

    Added some formatting to the output of the print function.

--- src/BindingList.c
+++ src/BindingList.c
@@ -47,13 +47,33 @@ void freeBindingList(BindingList* bindingList) {
 
 }
 
-void printBindingList( BindingList* lst ) {
+void printBindingListElements( BindingList* lst ) {
 
     printBinding( lst->binding );
 
     if( lst->prevBindings != NULL ) {
         printf("\n");
-        printBindingList(lst->prevBindings);
+        printBindingListElements(lst->prevBindings);
     }
+
+}
+
+void printBindingList( BindingList* lst ) {
+
+    printf( "\n----------------------------------------\n\n" );
+
+    printf("Binding List:\n");
+    printf("-------------\n");
+    
+    printf("\n");
+
+    printBindingListElements( lst );
+
+    printf("\n");
+    printf("\nEND\n");
+
+    printf( "\n----------------------------------------\n\n" );
+
+    
 
 }
